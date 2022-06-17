3706a0fb32fa400074a4f7e4114009167f698b2c
Hyunji <rioreo2022@gmail.com>
2022-06-17 17:45:58 +0900

    init: project setting

---
+++ aircnc/src/main/java/com/gurudev/aircnc/AircncApplication.java
@@ -0,0 +1,13 @@
+package com.gurudev.aircnc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AircncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AircncApplication.class, args);
+	}
+
+}
