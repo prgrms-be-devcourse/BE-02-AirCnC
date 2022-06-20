d586975bff106826de96427572d9a2e9e257b6dc
ndy2 <emrdbs12@gmail.com>
2022-06-20 15:58:57 +0900

    test: 숙소 엔티티 개발 [AIR-41]
    RoomTest, AddressTest 작성

--- aircnc/src/main/java/com/gurudev/aircnc/domain/room/Room.java
+++ aircnc/src/main/java/com/gurudev/aircnc/domain/room/Room.java
@@ -6,6 +6,7 @@ import static org.springframework.util.StringUtils.hasText;
 
 import com.gurudev.aircnc.domain.base.BaseIdEntity;
 import com.gurudev.aircnc.domain.member.entity.Member;
+import com.gurudev.aircnc.domain.member.entity.Role;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
@@ -16,6 +17,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 public class Room extends BaseIdEntity {
+
+  public static final int ROOM_DESCRIPTION_MIN_LENGTH = 10;
+  public static final int ROOM_PRICE_PER_DAY_MIN_VALUE = 10000;
+  public static final int ROOM_CAPACITY_MIN_VALUE = 1;
 
   private String name;
 
@@ -36,11 +41,18 @@ public class Room extends BaseIdEntity {
   public Room(String name, Address address, String description, int pricePerDay, int capacity,
       Member host) {
     checkArgument(hasText(name), "이름은 공백이 될 수 없습니다");
-    checkArgument(name.length() >=1, "이름은 1자 이상이어야 합니다");
+
     checkArgument(hasText(description), "설명은 공백이 될 수 없습니다");
-    checkArgument(description.length() >=10, "설명은 10자 이상이어야 합니다");
-    checkArgument(pricePerDay >= 10000, "가격은 10000원 이상이어야 합니다");
-    checkArgument(capacity>=1, "인원수는 1명 이상이어야 합니다");
+    checkArgument(description.length() >= ROOM_DESCRIPTION_MIN_LENGTH,
+        "설명은 %d 자 이상이어야 합니다".formatted(ROOM_DESCRIPTION_MIN_LENGTH));
+
+    checkArgument(pricePerDay >= ROOM_PRICE_PER_DAY_MIN_VALUE,
+        "가격은 %d원 이상이어야 합니다".formatted(ROOM_PRICE_PER_DAY_MIN_VALUE));
+
+    checkArgument(capacity >= ROOM_CAPACITY_MIN_VALUE,
+        "인원수는 %d명 이상이어야 합니다".formatted(ROOM_CAPACITY_MIN_VALUE));
+
+    checkArgument(host.getRole().equals(Role.HOST), "숙소 생성은 호스트만 할 수 있습니다");
 
     this.name = name;
     this.address = address;
---
+++ aircnc/src/test/java/com/gurudev/aircnc/domain/room/AddressTest.java
@@ -0,0 +1,26 @@
+package com.gurudev.aircnc.domain.room;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+class AddressTest {
+
+  @Test
+  void 주소_생성() {
+    Address address = new Address("전라북도 전주시 완산구 풍산동 3가");
+
+    assertThat(address).isEqualTo(new Address("전라북도 전주시 완산구 풍산동 3가"));
+  }
+
+  @ParameterizedTest
+  @NullAndEmptySource
+  void 공백인_주소_생성_실패(String address) {
+
+    assertThatIllegalArgumentException()
+        .isThrownBy(() -> new Address(address));
+  }
+}
