4d4144739691c5d8ef3f32a18e080e736e9c8e26
Hyunji <rioreo2022@gmail.com>
2022-06-21 14:53:16 +0900

    test : Trip test 수정 [AIR-86]
    - 생성자 -> ofReserved() 로 수정
    - 픽스처 필드로 변경

--- aircnc/src/test/java/com/gurudev/aircnc/domain/trip/entity/TripTest.java
+++ aircnc/src/test/java/com/gurudev/aircnc/domain/trip/entity/TripTest.java
@@ -2,11 +2,14 @@ package com.gurudev.aircnc.domain.trip.entity;
 
 import static com.gurudev.aircnc.domain.trip.entity.Trip.TRIP_HEADCOUNT_MIN_VALUE;
 import static com.gurudev.aircnc.domain.trip.entity.Trip.TRIP_TOTAL_PRICE_MIN_VALUE;
+import static com.gurudev.aircnc.domain.trip.entity.TripStatus.RESERVED;
+import static com.gurudev.aircnc.domain.trip.entity.TripStatus.TRAVELLING;
+import static java.time.LocalDate.now;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import com.gurudev.aircnc.domain.member.entity.Member;
-import com.gurudev.aircnc.domain.room.Room;
+import com.gurudev.aircnc.domain.room.entity.Room;
 import com.gurudev.aircnc.domain.util.Fixture;
 import java.time.LocalDate;
 import java.util.List;
@@ -14,51 +17,55 @@ import org.junit.jupiter.api.Test;
 
 class TripTest {
 
+  private final Member guest = Fixture.createGuest();
+  private final Room room = Fixture.createRoom();
+  private final LocalDate checkIn = now().plusDays(1);
+  private final LocalDate checkOut = now().plusDays(2);
+
   @Test
   void Trip_생성_성공() {
-    Member guest = Fixture.createGuest();
-    Room room = Fixture.createRoom();
-    LocalDate checkIn = LocalDate.now().plusDays(1);
-    LocalDate checkOut = LocalDate.now().plusDays(2);
-
-    Trip trip = new Trip(guest, room, checkIn, checkOut,
-        TRIP_TOTAL_PRICE_MIN_VALUE, TRIP_HEADCOUNT_MIN_VALUE, TripStatus.RESERVED);
+    Trip trip = Trip.ofReserved(guest, room, checkIn, checkOut, TRIP_TOTAL_PRICE_MIN_VALUE,
+        TRIP_HEADCOUNT_MIN_VALUE);
 
     assertThat(trip).extracting(Trip::getGuest, Trip::getRoom, Trip::getCheckIn, Trip::getCheckOut,
             Trip::getTotalPrice, Trip::getHeadCount, Trip::getStatus)
-        .isEqualTo(List.of(guest, room, TRIP_TOTAL_PRICE_MIN_VALUE, TRIP_HEADCOUNT_MIN_VALUE,
-            TripStatus.RESERVED));
+        .isEqualTo(List.of(guest, room, checkIn, checkOut, TRIP_TOTAL_PRICE_MIN_VALUE,
+            TRIP_HEADCOUNT_MIN_VALUE, RESERVED));
+  }
+
+  @Test
+  void Trip_status를_변경할_수_있다() {
+    Trip trip = Trip.ofReserved(guest, room, checkIn, checkOut, TRIP_TOTAL_PRICE_MIN_VALUE,
+        TRIP_HEADCOUNT_MIN_VALUE);
+
+    trip.changeStatus(TRAVELLING);
+
+    assertThat(trip).extracting(Trip::getGuest, Trip::getRoom, Trip::getCheckIn, Trip::getCheckOut,
+            Trip::getTotalPrice, Trip::getHeadCount, Trip::getStatus)
+        .isEqualTo(List.of(guest, room, checkIn, checkOut, TRIP_TOTAL_PRICE_MIN_VALUE,
+            TRIP_HEADCOUNT_MIN_VALUE, TRAVELLING));
   }
 
   @Test
   void CheckIn이_CheckOut_이전_이여야_한다() {
-    Member guest = Fixture.createGuest();
-    Room room = Fixture.createRoom();
-
     assertThatIllegalArgumentException()
-        .isThrownBy(() -> new Trip(guest, room, LocalDate.now(), LocalDate.now().minusDays(1),
-            TRIP_TOTAL_PRICE_MIN_VALUE, TRIP_HEADCOUNT_MIN_VALUE, TripStatus.RESERVED));
+        .isThrownBy(() -> Trip.ofReserved(guest, room, now(), now().minusDays(1),
+            TRIP_TOTAL_PRICE_MIN_VALUE, TRIP_HEADCOUNT_MIN_VALUE));
   }
 
   @Test
   void 인원이_제한_인원_이하_일_수_없다() {
-    Member guest = Fixture.createGuest();
-    Room room = Fixture.createRoom();
-
     assertThatIllegalArgumentException()
         .isThrownBy(
-            () -> new Trip(guest, room, LocalDate.now().plusDays(1), LocalDate.now().plusDays(2),
-                TRIP_TOTAL_PRICE_MIN_VALUE, TRIP_HEADCOUNT_MIN_VALUE - 1, TripStatus.RESERVED));
+            () -> Trip.ofReserved(guest, room, now().plusDays(1), now().plusDays(2),
+                TRIP_TOTAL_PRICE_MIN_VALUE, TRIP_HEADCOUNT_MIN_VALUE - 1));
   }
 
   @Test
   void 총_가격은_제한가격_미만일_수_없다() {
-    Member guest = Fixture.createGuest();
-    Room room = Fixture.createRoom();
-
     assertThatIllegalArgumentException()
         .isThrownBy(
-            () -> new Trip(guest, room, LocalDate.now().plusDays(1), LocalDate.now().plusDays(2),
-                TRIP_TOTAL_PRICE_MIN_VALUE - 1, TRIP_HEADCOUNT_MIN_VALUE, TripStatus.RESERVED));
+            () -> Trip.ofReserved(guest, room, now().plusDays(1), now().plusDays(2),
+                TRIP_TOTAL_PRICE_MIN_VALUE - 1, TRIP_HEADCOUNT_MIN_VALUE));
   }
 }
