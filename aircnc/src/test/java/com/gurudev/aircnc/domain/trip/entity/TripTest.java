ced5c3335acd6dd4a233b7c242707783695818fe
ndy2 <emrdbs12@gmail.com>
2022-06-23 17:18:03 +0900

    test: 여행 취소 도메인 테스트 작성 [AIR-96]

--- aircnc/src/test/java/com/gurudev/aircnc/domain/trip/entity/TripTest.java
+++ aircnc/src/test/java/com/gurudev/aircnc/domain/trip/entity/TripTest.java
@@ -2,19 +2,22 @@ package com.gurudev.aircnc.domain.trip.entity;
 
 import static com.gurudev.aircnc.domain.trip.entity.Trip.TRIP_HEADCOUNT_MIN_VALUE;
 import static com.gurudev.aircnc.domain.trip.entity.Trip.TRIP_TOTAL_PRICE_MIN_VALUE;
+import static com.gurudev.aircnc.domain.trip.entity.TripStatus.CANCELLED;
 import static com.gurudev.aircnc.domain.trip.entity.TripStatus.RESERVED;
 import static com.gurudev.aircnc.domain.trip.entity.TripStatus.TRAVELLING;
 import static com.gurudev.aircnc.domain.util.Fixture.createGuest;
 import static com.gurudev.aircnc.domain.util.Fixture.createRoom;
+import static com.gurudev.aircnc.domain.util.Fixture.createTrip;
 import static com.gurudev.aircnc.util.AssertionUtil.assertThatAircncRuntimeException;
 import static com.gurudev.aircnc.util.AssertionUtil.assertThatTripReservationException;
 import static java.time.LocalDate.now;
 import static java.time.Period.between;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import com.gurudev.aircnc.domain.member.entity.Member;
 import com.gurudev.aircnc.domain.room.entity.Room;
+import com.gurudev.aircnc.exception.TripCancelException;
 import java.time.LocalDate;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,15 +25,20 @@ import org.junit.jupiter.api.Test;
 
 class TripTest {
 
-  private final Member guest = createGuest();
-  private final Room room = createRoom();
+  private Member guest;
+  private Room room;
+
   private LocalDate checkIn;
   private LocalDate checkOut;
+
   private int totalPrice;
   private int headCount;
 
   @BeforeEach
   void setUp() {
+    guest = createGuest();
+    room = createRoom();
+
     checkIn = now().plusDays(1);
     checkOut = now().plusDays(2);
 
@@ -60,8 +68,7 @@ class TripTest {
   void CheckIn이_CheckOut_이전_이여야_한다() {
     assertThatAircncRuntimeException()
         .isThrownBy(
-            () -> Trip.ofReserved(guest, room, checkIn, checkIn.minusDays(1), totalPrice,
-                headCount));
+            () -> Trip.ofReserved(guest, room, checkIn, checkIn.minusDays(1), totalPrice, headCount));
   }
 
   @Test
@@ -92,5 +99,23 @@ class TripTest {
     assertThatTripReservationException()
         .isThrownBy(() -> Trip.ofReserved(guest, room, checkIn, checkOut, totalPrice,
             room.getCapacity() + 1));
+  }
+
+  @Test
+  void 예약_상태의_여행_취소_성공() {
+    Trip trip = createTrip();
+
+    trip.cancel();
+
+    assertThat(trip.getStatus()).isEqualTo(CANCELLED);
+  }
+
+  @Test
+  void 취소_상태의_여행_취소_실패() {
+    Trip trip = createTrip();
+    trip.cancel(); // -> trip.status = CANCELLED
+
+    assertThatExceptionOfType(TripCancelException.class)
+        .isThrownBy(trip::cancel);
   }
 }
