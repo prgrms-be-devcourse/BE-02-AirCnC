395e15b72bd6895694a0e4b53bdcf20b8d3567c4
HyunJi <rioreo2022@gmail.com>
2022-06-27 18:41:26 +0900

    test: RoomServiceImplTest update 테스트 [AIR-57]
    - 숙소 이름 설명 가격 변경 성공
    - 해당 숙소의 호스트가 아닌 경우 변경 실패

--- aircnc/src/test/java/com/gurudev/aircnc/domain/room/service/RoomServiceImplTest.java
+++ aircnc/src/test/java/com/gurudev/aircnc/domain/room/service/RoomServiceImplTest.java
@@ -3,17 +3,21 @@ package com.gurudev.aircnc.domain.room.service;
 import static com.gurudev.aircnc.domain.util.Command.ofRoom;
 import static com.gurudev.aircnc.domain.util.Fixture.createRoom;
 import static com.gurudev.aircnc.domain.util.Fixture.createRoomPhoto;
+import static com.gurudev.aircnc.util.AssertionUtil.assertThatNotFoundException;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.gurudev.aircnc.domain.member.entity.Member;
 import com.gurudev.aircnc.domain.member.service.MemberService;
 import com.gurudev.aircnc.domain.room.entity.Room;
 import com.gurudev.aircnc.domain.room.entity.RoomPhoto;
+import com.gurudev.aircnc.domain.room.service.command.RoomCommand.RoomUpdateCommand;
 import com.gurudev.aircnc.domain.util.Command;
 import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -44,8 +48,7 @@ class RoomServiceImplTest {
     roomPhotos = List.of(createRoomPhoto(), createRoomPhoto());
 
     room1 = roomService.register(ofRoom(room1, roomPhotos, host.getId()));
-    room2 = roomService.register(
-        ofRoom(room2, Collections.emptyList(), host.getId()));
+    room2 = roomService.register(ofRoom(room2, Collections.emptyList(), host.getId()));
   }
 
   @Test
@@ -66,5 +69,47 @@ class RoomServiceImplTest {
 
     assertThat(rooms).hasSize(2)
         .containsExactly(room1, room2);
+  }
+
+  @ParameterizedTest
+  @CsvSource(value = {
+      "변경된 숙소 이름, 변경된 숙소 설명입니다, 25000",
+      "변경된 숙소 이름, , ",
+      " , 변경된 숙소 설명입니다, ",
+      " , , 25000"
+  })
+  void 숙소_이름_설명_가격_변경_성공(String updatedName, String updatedDescription,
+      Integer updatedPricePerDay) {
+    //given
+    String originalName = room1.getName();
+    String originalDescription = room1.getDescription();
+    Integer originalPricePerDay = room1.getPricePerDay();
+    RoomUpdateCommand roomUpdateCommand = new RoomUpdateCommand(room1.getHost().getId(),
+        room1.getId(), updatedName, updatedDescription, updatedPricePerDay);
+
+    //when
+    Room updatedRoom = roomService.update(roomUpdateCommand);
+
+    //then
+    assertThat(updatedRoom)
+        .extracting(
+            Room::getName,
+            Room::getDescription,
+            Room::getPricePerDay
+        ).isEqualTo(
+            List.of(updatedName == null ? originalName : updatedName,
+                updatedDescription == null ? originalDescription : updatedDescription,
+                updatedPricePerDay == null ? originalPricePerDay : updatedPricePerDay));
+  }
+
+  @Test
+  void 해당_숙소의_호스트가_아닌_경우_변경_실패() {
+    //given
+    RoomUpdateCommand roomUpdateCommand = new RoomUpdateCommand(host.getId() + 1,
+        room1.getId(), "변경할 숙소 이름", "변경할 숙소 설명입니다", 25000);
+
+    //then
+    assertThatNotFoundException()
+        .isThrownBy(() -> roomService.update(roomUpdateCommand));
   }
 }
