77a9bff094b72952d80a3e6a8199b06fe0ade621
Hyunji <rioreo2022@gmail.com>
2022-06-28 02:00:58 +0900

    feat: 숙소 삭제 서비스 구현 [AIR-60]
    - RoomServiceImpl delete
    - RoomDeleteException 추가

--- aircnc/src/main/java/com/gurudev/aircnc/domain/room/service/RoomService.java
+++ aircnc/src/main/java/com/gurudev/aircnc/domain/room/service/RoomService.java
@@ -1,6 +1,7 @@
 package com.gurudev.aircnc.domain.room.service;
 
 import com.gurudev.aircnc.domain.room.entity.Room;
+import com.gurudev.aircnc.domain.room.service.command.RoomCommand.RoomDeleteCommand;
 import com.gurudev.aircnc.domain.room.service.command.RoomCommand.RoomRegisterCommand;
 import com.gurudev.aircnc.domain.room.service.command.RoomCommand.RoomUpdateCommand;
 import java.util.List;
@@ -12,4 +13,8 @@ public interface RoomService {
   List<Room> getAll();
 
   Room update(RoomUpdateCommand roomUpdateCommand);
+
+  void delete(RoomDeleteCommand roomDeleteCommand);
+
+  Room getById(Long id);
 }
---
+++ aircnc/src/main/java/com/gurudev/aircnc/exception/RoomDeleteException.java
@@ -0,0 +1,8 @@
+package com.gurudev.aircnc.exception;
+
+public class RoomDeleteException extends AircncRuntimeException {
+
+  public RoomDeleteException(String message) {
+    super(message);
+  }
+}
