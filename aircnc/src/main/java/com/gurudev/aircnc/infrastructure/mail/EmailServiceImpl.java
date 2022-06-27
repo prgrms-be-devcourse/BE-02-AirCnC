f27e51eee06ca86c5494436a3bc4f80d08a1bde4
geuno <rlfrmsdh1@gmail.com>
2022-06-27 21:16:01 +0900

    feat: Thymeleaf를 이용한 메시지 작성 [AIR-111]  - 간단한 이메일 기능 테스트 코드 추가

--- aircnc/src/main/java/com/gurudev/aircnc/infrastructure/mail/EmailServiceImpl.java
+++ aircnc/src/main/java/com/gurudev/aircnc/infrastructure/mail/EmailServiceImpl.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
 
 @Component
 public class EmailServiceImpl implements EmailService {
@@ -15,31 +18,23 @@ public class EmailServiceImpl implements EmailService {
   @Autowired
   JavaMailSender emailSender;
 
-  public static final String ePw = createKey();
+  @Autowired
+  private SpringTemplateEngine springTemplateEngine;
 
-  private MimeMessage createMessage(String to) throws Exception {
-    System.out.println("보내는 대상 : " + to);
-    System.out.println("인증 번호 : " + ePw);
+  public static final String authenticationKey = createKey();
+
+  @Override
+  public MimeMessage createSignUpMsg(String to) throws Exception {
     MimeMessage message = emailSender.createMimeMessage();
 
     message.addRecipients(RecipientType.TO, to); //보내는 대상
     message.setSubject("AirCnC 회원가입 이메일 인증"); //제목
 
-    String msgg = "";
-    msgg += "<div style='margin:100px;'>";
-    msgg += "<h1> 안녕하세요 AirCnC 입니다. </h1>";
-    msgg += "<br>";
-    msgg += "<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요<p>";
-    msgg += "<br>";
-    msgg += "<p>감사합니다!<p>";
-    msgg += "<br>";
-    msgg += "<div align='center' style='border:1px solid black; font-family:verdana';>";
-    msgg += "<h3 style='color:blue;'>회원가입 인증 코드입니다.</h3>";
-    msgg += "<div style='font-size:130%'>";
-    msgg += "CODE : <strong>";
-    msgg += ePw + "</strong><div><br/> ";
-    msgg += "</div>";
-    message.setText(msgg, "utf-8", "html"); //내용
+    Context context = new Context();
+    context.setVariable("code", authenticationKey);
+    String msg = springTemplateEngine.process("register-member", context);
+
+    message.setText(msg, "utf-8", "html"); //내용
     message.setFrom(new InternetAddress("aircnc@gmail.com", "AirCnC")); //보내는 사람
 
     return message;
@@ -67,19 +62,16 @@ public class EmailServiceImpl implements EmailService {
           break;
       }
     }
-
     return key.toString();
   }
 
   @Override
-  public String sendSimpleMessage(String to) throws Exception {
-    MimeMessage message = createMessage(to);
-    try {//예외처리
+  public void sendMessage(MimeMessage message) {
+    try { //예외처리
       emailSender.send(message);
     } catch (MailException es) {
       es.printStackTrace();
       throw new IllegalArgumentException();
     }
-    return ePw;
   }
 }
