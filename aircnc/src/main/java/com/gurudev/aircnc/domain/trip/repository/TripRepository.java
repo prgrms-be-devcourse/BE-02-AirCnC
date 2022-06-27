e78a4eafa0b4bdc07a6a201836e911861426404e
Hyunji <rioreo2022@gmail.com>
2022-06-28 01:59:01 +0900

    feat: TripRepository, RoomRepository 쿼리 작성 [AIR-60]
    - findByRoomIdAndStatusSet
    - deleteByIdAndHostId

--- aircnc/src/main/java/com/gurudev/aircnc/domain/trip/repository/TripRepository.java
+++ aircnc/src/main/java/com/gurudev/aircnc/domain/trip/repository/TripRepository.java
@@ -3,8 +3,11 @@ package com.gurudev.aircnc.domain.trip.repository;
 import com.gurudev.aircnc.domain.member.entity.Member;
 import com.gurudev.aircnc.domain.trip.entity.Trip;
 import java.time.LocalDate;
+import com.gurudev.aircnc.domain.trip.entity.TripStatus;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -27,6 +30,11 @@ public interface TripRepository extends JpaRepository<Trip, Long> {
       + "join fetch t.guest "
       + "where t.id = :id")
   Optional<Trip> findByIdFetchGuest(Long id);
+
+  @Query("select t "
+      + "from Trip t "
+      + "where t.room.id = :roomId and t.status in :statusSet")
+  List<Trip> findByRoomIdAndStatusSet(Long roomId, Set<TripStatus> statusSet, Pageable pageable);
 
   @Modifying(clearAutomatically = true)
   @Query("update Trip t "
