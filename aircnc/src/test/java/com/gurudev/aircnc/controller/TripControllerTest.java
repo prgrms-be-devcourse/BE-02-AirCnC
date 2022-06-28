0086246a6674bbdc4d08a149cd5cc6c2220717b8
ndy2 <emrdbs12@gmail.com>
2022-06-28 23:15:41 +0900

    test: 여행 취소 API 테스트 [AIR-91]

--- aircnc/src/test/java/com/gurudev/aircnc/controller/TripControllerTest.java
+++ aircnc/src/test/java/com/gurudev/aircnc/controller/TripControllerTest.java
@@ -150,7 +150,7 @@ class TripControllerTest extends RestDocsTestSupport {
     Long roomId = 숙소_등록("나의 숙소", new Address("달나라 1번지", "달나라 1길", "100호", "1234"),
         "달토끼가 사는 나의 숙소", "100000", "2");
 
-    //조회할 여행 등록
+    //상세 조회할 여행 등록
     로그인("guest@naver.com", "guest1234!");
     Long tripId = 여행_등록(checkIn, checkOut, 100000, 2, roomId);
 
@@ -196,4 +196,53 @@ class TripControllerTest extends RestDocsTestSupport {
         );
   }
 
+  @Test
+  void 여행_취소_성공() throws Exception {
+    //given
+    //여행 필드
+    String checkIn = now().toString();
+    String checkOut = now().plusDays(1).toString();
+
+    //숙소 세팅
+    로그인("host@naver.com", "host1234!");
+    Long roomId = 숙소_등록("나의 숙소", new Address("달나라 1번지", "달나라 1길", "100호", "1234"),
+        "달토끼가 사는 나의 숙소", "100000", "2");
+
+    //취소할 여행 등록
+    로그인("guest@naver.com", "guest1234!");
+    Long tripId = 여행_등록(checkIn, checkOut, 100000, 2, roomId);
+
+    //when
+    mockMvc.perform(post("/api/v1/trips/{tripId}/cancel", tripId)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header(AUTHORIZATION, token))
+        //then
+        .andExpect(status().isOk())
+        .andExpectAll(
+            jsonPath("$.trip.id").value(tripId),
+            jsonPath("$.trip.checkIn").value(checkIn),
+            jsonPath("$.trip.checkOut").value(checkOut),
+            jsonPath("$.trip.totalPrice").value("100000"),
+            jsonPath("$.trip.headCount").value("2"),
+            jsonPath("$.trip.status").value("CANCELLED"),
+            jsonPath("$.trip.roomId").value(roomId)
+        )
+        //docs
+        .andDo(
+            restDocs.document(
+                requestHeaders(
+                    headerWithName(AUTHORIZATION).description("인증 토큰")
+                ),
+                responseFields(
+                    fieldWithPath("trip.id").type(NUMBER).description("여행 아이디"),
+                    fieldWithPath("trip.checkIn").type(STRING).description("체크인 날짜"),
+                    fieldWithPath("trip.checkOut").type(STRING).description("체크아웃 날짜"),
+                    fieldWithPath("trip.totalPrice").type(NUMBER).description("총 가격"),
+                    fieldWithPath("trip.headCount").type(NUMBER).description("인원 수"),
+                    fieldWithPath("trip.status").type(STRING).description("여행 상태 (항상 CANCELLED)"),
+                    fieldWithPath("trip.roomId").type(NUMBER).description("숙소 아이디")
+                )
+            )
+        );
+  }
 }
