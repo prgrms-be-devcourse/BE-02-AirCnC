500de428fa922c09b310cf965809f83e65a9df8e
Hyunji <rioreo2022@gmail.com>
2022-06-28 18:01:36 +0900

    feat: 숙소 변경 API [AIR-58]
    - RoomDto 에 RoomUpdateRequest, Response 추가

--- aircnc/src/main/java/com/gurudev/aircnc/controller/RoomController.java
+++ aircnc/src/main/java/com/gurudev/aircnc/controller/RoomController.java
@@ -1,15 +1,19 @@
 package com.gurudev.aircnc.controller;
 
 import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 import com.gurudev.aircnc.controller.dto.RoomDto.RoomRegisterRequest;
 import com.gurudev.aircnc.controller.dto.RoomDto.RoomRegisterResponse;
+import com.gurudev.aircnc.controller.dto.RoomDto.RoomUpdateRequest;
+import com.gurudev.aircnc.controller.dto.RoomDto.RoomUpdateResponse;
 import com.gurudev.aircnc.domain.base.AttachedFile;
 import com.gurudev.aircnc.domain.room.entity.Room;
 import com.gurudev.aircnc.domain.room.entity.RoomPhoto;
 import com.gurudev.aircnc.domain.room.service.RoomPhotoService;
 import com.gurudev.aircnc.domain.room.service.RoomService;
 import com.gurudev.aircnc.domain.room.service.command.RoomCommand.RoomRegisterCommand;
+import com.gurudev.aircnc.domain.room.service.command.RoomCommand.RoomUpdateCommand;
 import com.gurudev.aircnc.infrastructure.security.jwt.JwtAuthentication;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,7 +22,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,4 +59,16 @@ public class RoomController {
     return new ResponseEntity<>(RoomRegisterResponse.of(room, roomPhotos), CREATED);
   }
 
+  @PatchMapping("/{roomId}")
+  public ResponseEntity<RoomUpdateResponse> updateRoom(
+      @AuthenticationPrincipal JwtAuthentication authentication,
+      @RequestBody RoomUpdateRequest request,
+      @PathVariable("roomId") Long roomId) {
+
+    Room room = roomService.update(
+        new RoomUpdateCommand(authentication.id, roomId, request.getName(),
+            request.getDescription(), request.getPricePerDay()));
+
+    return new ResponseEntity<>(RoomUpdateResponse.of(room, room.getRoomPhotos()), OK);
+  }
 }
