8d151f3a304035f83afefe62114cbbe82542489e
geuno <rlfrmsdh1@gmail.com>
2022-07-04 21:29:17 +0900

    refactor: toMap 테스트에도 적용 [AIR-136]

--- aircnc/src/test/java/com/gurudev/aircnc/infrastructure/mail/EmailServiceTest.java
+++ aircnc/src/test/java/com/gurudev/aircnc/infrastructure/mail/EmailServiceTest.java
@@ -1,5 +1,7 @@
 package com.gurudev.aircnc.infrastructure.mail;
 
+import static com.gurudev.aircnc.domain.utils.MapUtils.toMap;
+
 import com.gurudev.aircnc.domain.room.entity.Room;
 import com.gurudev.aircnc.domain.trip.entity.Trip;
 import com.gurudev.aircnc.domain.util.Fixture;
@@ -18,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 @Disabled
 class EmailServiceTest { // 사용방법을 위한 Temporary 테스트
 
+
   @Autowired
   @Qualifier("memberEmailService")
   private EmailService memberEmailService;
@@ -29,6 +32,7 @@ class EmailServiceTest { // 사용방법을 위한 Temporary 테스트
   @Autowired
   @Qualifier("tripEmailService")
   private EmailService tripEmailService;
+
 
   private Room room;
 
@@ -50,28 +54,28 @@ class EmailServiceTest { // 사용방법을 위한 Temporary 테스트
   @Test
   void 숙소_삭제_이메일_전송_테스트() throws InterruptedException {
     roomEmailService.send("rlfrmsdh1@gmail.com",
-        room.toMap(), MailType.DELETE);
-    Thread.sleep(5000); // 비동기여서 전송될때까지 기다려주는 로직
+        toMap(room), MailType.DELETE);
+    Thread.sleep(5000);
   }
 
   @Test
   void 숙소_등록_이메일_전송_테스트() throws InterruptedException {
     roomEmailService.send("rlfrmsdh1@gmail.com",
-        room.toMap(), MailType.REGISTER);
+        toMap(room), MailType.REGISTER);
     Thread.sleep(5000);
   }
 
   @Test
   void 숙소_변경_이메일_전송_테스트() throws InterruptedException {
     roomEmailService.send("rlfrmsdh1@gmail.com",
-        room.toMap(), MailType.UPDATE);
+        toMap(room), MailType.UPDATE);
     Thread.sleep(5000);
   }
 
   @Test
   void 여행_변경_이메일_전송_테스트() throws InterruptedException {
     tripEmailService.send("rlfrmsdh1@gmail.com",
-        trip.toMap(), MailType.UPDATE);
+        toMap(trip), MailType.UPDATE);
     Thread.sleep(5000);
   }
 }
