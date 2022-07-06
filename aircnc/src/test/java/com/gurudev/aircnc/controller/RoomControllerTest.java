312e71b4bb83fcbcc076acab773046b889dee199
Juhan <juhan211@naver.com>
2022-07-06 09:37:33 +0900

    feature: 숙소 상세 조회 Controller 테스트 생성 [AIR-55]

--- aircnc/src/test/java/com/gurudev/aircnc/controller/RoomControllerTest.java
+++ aircnc/src/test/java/com/gurudev/aircnc/controller/RoomControllerTest.java
@@ -1,20 +1,35 @@
 package com.gurudev.aircnc.controller;
 
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.payload.JsonFieldType.ARRAY;
 import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
 import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.gurudev.aircnc.controller.support.RestDocsTestSupport;
 import com.gurudev.aircnc.domain.room.entity.Address;
+import com.gurudev.aircnc.domain.trip.entity.Trip;
+import com.gurudev.aircnc.domain.trip.service.TripService;
+import com.gurudev.aircnc.infrastructure.event.TripEvent;
+import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
 class RoomControllerTest extends RestDocsTestSupport {
+
+  @Autowired
+  private TripService tripService;
 
   @Test
   void 모든_숙소_조회() throws Exception {
@@ -55,5 +70,45 @@ class RoomControllerTest extends RestDocsTestSupport {
                 fieldWithPath("rooms[].fileNames").type(ARRAY).description("등록된 파일의 이름")
             ))
         );
+  }
+
+
+  @Test
+  void 숙소_상세_조회() throws Exception {
+    //given
+    //숙소 등록
+    로그인("host@naver.com", "host1234!");
+    Long roomId = 숙소_등록("나의 숙소", new Address("달나라 1번지", "달나라 1길", "100호", "1234"),
+        "달토끼가 사는 나의 숙소", "100000", "2");
+
+    //여행 등록
+    Long guestId = 로그인("guest@naver.com", "guest1234!");
+    여행_등록(LocalDate.now().plusDays(0), LocalDate.now().plusDays(1),100000,2,roomId,guestId);
+    여행_등록(LocalDate.now().plusDays(2), LocalDate.now().plusDays(4),200000,2,roomId,guestId);
+
+    //when
+    mockMvc.perform(get("/api/v1/rooms/{roomId}",roomId))
+
+      //then
+      .andExpect(status().isOk())
+      .andExpectAll(
+        jsonPath("$.room.name").value("나의 숙소"),
+        jsonPath("$.room.address").value("달나라 1길 100호"),
+        jsonPath("$.room.pricePerDay").value("100000"),
+        jsonPath("$.room.capacity").value("2"),
+        jsonPath("$.room.unAvailableDays",hasSize(4)),
+        jsonPath("$.room.photoUrls",hasSize(1)),
+        jsonPath("$.room.description").value("달토끼가 사는 나의 숙소")
+    );
+  }
+
+  private Long 여행_등록(LocalDate checkIn, LocalDate checkOut,
+      int totalPrice, int headCount, Long roomId, Long guestId) {
+
+    Trip reservedTrip = tripService.reserve(
+        new TripEvent(guestId, roomId, checkIn, checkOut, headCount, totalPrice));
+
+    return reservedTrip.getId();
+
   }
 }
