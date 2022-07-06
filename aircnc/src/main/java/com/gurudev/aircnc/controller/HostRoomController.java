5506ef986a0b527219967377daa0db3e4c27f78b
ndy2 <emrdbs12@gmail.com>
2022-07-06 18:02:33 +0900

    refactor: todo 제거 [AIR-144]

--- aircnc/src/main/java/com/gurudev/aircnc/controller/HostRoomController.java
+++ aircnc/src/main/java/com/gurudev/aircnc/controller/HostRoomController.java
@@ -21,7 +21,6 @@ import com.gurudev.aircnc.infrastructure.security.jwt.JwtAuthentication;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -36,7 +35,6 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@Slf4j
 @RestController
 @RequestMapping("/api/v1/hosts/rooms")
 @RequiredArgsConstructor
